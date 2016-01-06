f0bf47fbe85aab9adc09106593e8fdd27ed76c7a
Mirko van der Waal <mvdw@airmail.cc>
2016-01-06 14:12:52 +0100

    Initial commit.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Short description for index.php
+ *
+ * @package index
+ * @author mvdw <mvdw at airmail dot cc>
+ * @version 0.1
+ * @copyright (C) 2016 mvdw <mvdw at airmail dot cc>
+ * @license MIT
+ */
+
+echo 'test';
+?>
